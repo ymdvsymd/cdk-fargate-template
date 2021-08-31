8727b92f7496c01b9dbc6a5954672795b4817a0b
chaser-at-its <tiw@outlook.jp>
2021-08-31 18:09:14 +0900

    update

--- bin/cdk-fargate-template.ts
+++ bin/cdk-fargate-template.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { DomainStack } from '../lib/domain-stack';
 import { NetworkStack } from '../lib/network-stack';
 import { ComputeStack } from '../lib/compute-stack';
 import { Context } from '../lib/context'
 
 const app = new cdk.App();
 const context = new Context(app.node);
-const network = new NetworkStack(app, `${context.stackNamePrefix}Network`);
-new ComputeStack(app, `${context.stackNamePrefix}Compute`, context, { vpc: network.vpc });
+
+// DomainStackのみ単独で実行する必要あり
+const domain = new DomainStack(app, `${context.stackPrefix}Domain`, context);
+
+const network = new NetworkStack(app, `${context.stackPrefix}Network`);
+new ComputeStack(app, `${context.stackPrefix}Compute`, { context, vpc: network.vpc, hostedZone: domain.hostedZone });
--- lib/compute-stack.ts
+++ lib/compute-stack.ts
@@ -1,37 +1,52 @@
 import * as cdk from '@aws-cdk/core';
 import { Vpc } from '@aws-cdk/aws-ec2';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as certificatemanager from '@aws-cdk/aws-certificatemanager';
 import * as ecs from '@aws-cdk/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 import { ApplicationProtocol } from '@aws-cdk/aws-elasticloadbalancingv2';
 import '../lib/string';
 import { Context } from '../lib/context'
 
+declare type Dependencies = {
+  context: Context,
+  vpc: Vpc,
+  hostedZone: route53.HostedZone
+};
+
 export class ComputeStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, context: Context, network: { vpc: Vpc }, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, deps: Dependencies, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const cluster = new ecs.Cluster(this, 'Cluster', { vpc: network.vpc });
+    const certificate = new certificatemanager.DnsValidatedCertificate(this, 'Certificate', {
+      domainName: `*.${deps.context.subDomain}`,
+      hostedZone: deps.hostedZone,
+      validationMethod: certificatemanager.ValidationMethod.DNS
+    });
+
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc: deps.vpc });
 
     const taskDef = new ecs.FargateTaskDefinition(this, 'TaskDef', {
       cpu: 256,
       memoryLimitMiB: 512
     });
-    const container = taskDef.addContainer(context.imageRepo.upperCamelCase(), {
-      image: ecs.ContainerImage.fromRegistry(context.imageRepo),
+    const container = taskDef.addContainer(deps.context.imageRepo.upperCamelCase(), {
+      image: ecs.ContainerImage.fromRegistry(deps.context.imageRepo),
       logging: new ecs.AwsLogDriver({ streamPrefix: 'EcsLogs' })
     });
     container.addPortMappings({
       containerPort: 80
-    })
+    });
 
     const fargateService = new ApplicationLoadBalancedFargateService(this, 'Service', {
       cluster: cluster,
       taskDefinition: taskDef,
       desiredCount: 2,
       listenerPort: 443,
-      protocol: ApplicationProtocol.HTTP,
-      // domainName: '',
-      // domainZone: undefined,
+      protocol: ApplicationProtocol.HTTPS,
+      domainName: deps.context.subDomain,
+      domainZone: deps.hostedZone,
+      certificate: certificate,
       // FargatePlatformVersion.VERSION1_4だとタスクがPENDINGのまま停止する
       platformVersion: ecs.FargatePlatformVersion.VERSION1_3
     });
